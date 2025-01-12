a41b61acc6f0ab23886f754cf829e456c7824b82
Liam <137022093+LiamPatLawless@users.noreply.github.com>
2025-01-12 11:33:59 +1000

    Functioning draggable object script

--- Scenes/Level/Draggable.gd
+++ Scenes/Level/Draggable.gd
@@ -2,13 +2,18 @@ extends Interaction
 class_name Draggable
 
 @export var camera : Camera3D
+@export_flags_3d_physics var collisionMask
+@export var debugObj : Node3D
 
 var mousePos : Vector3
 var lerpStartPos : Vector3
-var tempPosDeleteLater : Vector3
 
 var pinned : bool
 var lerpValue : float
+var mouseOffset : Vector3
+
+var dragPos : Vector3
+var dragDir : Vector3
 
 var raycastHit
 
@@ -18,44 +23,25 @@ func _ready() -> void:
 	mouse_exited.connect(_on_mouse_exited)
 	
 	lerpStartPos = self.global_position
-	tempPosDeleteLater = self.global_position
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	# If the object is currently draggable
 	if GlobalSignalPipe.current_draggable == self:
-		# Set the mouse position to the current mouse position
-		var mousePos = camera.project_position(get_viewport().get_mouse_position(), camera.global_transform.origin.z * 2)
-		var mousePos2D = get_viewport().get_mouse_position()
-
+		# Do a raycast
+		raycastHit = raycast_from_mouse()
 		
-		# Create a raycast from the mouse position outwards (why is this so cumbersome)
-		var space = get_world_3d().direct_space_state
-		var rayStart = camera.project_ray_origin(mousePos2D)
-		var rayEnd = rayStart + camera.project_ray_normal(mousePos2D) * 100 # cast for 100m
-		var rayQuery = PhysicsRayQueryParameters3D.create(rayStart, rayEnd, 4294967295 - 3, [self])
-		rayQuery.collide_with_areas = true
-		#rayQuery.exclude = [self, $CSGBox3D, $CollisionShape3D]
-		raycastHit = space.intersect_ray(rayQuery)
-		
-		print(raycastHit)
-		
-		var dragPos : Vector3
-		
+		#If it was successful
 		if not raycastHit.is_empty():
-			#var directionToHit = raycastHit.position - camera.position
-			#dragPos = raycastHit.position - directionToHit * 0
-			dragPos = Vector3(mousePos.x, mousePos.y, mousePos.z)
-		else:
-			dragPos = Vector3(mousePos.x, mousePos.y, mousePos.z)
+			dragPos = raycastHit.position
+			dragDir = raycastHit.normal
+			debugObj.position = dragPos
+			debugObj.transform = debugObj.transform.looking_at(debugObj.position + dragDir)
 		
 		# Lerp the paper towards the mouse until it reaches it, then lock it to the mouse position relative to the camera
 		self.global_position = lerpStartPos.lerp(dragPos, lerpValue)
 		lerpValue = clamp(lerpValue + delta * 8, 0, 1)
-	else:
-		self.global_position = lerpStartPos.lerp(tempPosDeleteLater, lerpValue)
-		lerpValue = clamp(lerpValue + delta * 10, 0, 1)
 
 
 func _on_mouse_entered() -> void:
@@ -79,7 +65,9 @@ func interact(body):
 		# Start drag lerp
 		lerpValue = 0
 		lerpStartPos = self.global_position
-		tempPosDeleteLater = self.global_position
+
+		raycastHit = raycast_from_mouse()
+		mouseOffset = raycastHit.position - lerpStartPos 
 	return "continue"
 
 func forceStopInteract(body):
@@ -94,3 +82,18 @@ func forceStopInteract(body):
 		lerpValue = 0
 		lerpStartPos = self.global_position
 	return "continue"
+
+func raycast_from_mouse():
+	# Set the mouse position to the current mouse position
+	var mousePos = camera.project_position(get_viewport().get_mouse_position(), camera.global_transform.origin.z * 2)
+	var mousePos2D = get_viewport().get_mouse_position()
+	
+	# Create a raycast from the mouse position outwards (why is this so cumbersome)
+	var space = get_world_3d().direct_space_state
+	var rayStart = camera.project_ray_origin(mousePos2D)
+	var rayEnd = rayStart + camera.project_ray_normal(mousePos2D) * 100 # cast for 100m
+	var rayQuery = PhysicsRayQueryParameters3D.create(rayStart, rayEnd, collisionMask, [self])		
+	raycastHit = space.intersect_ray(rayQuery)
+	
+	return raycastHit
+	
